ba6f33c68dbbb6349f325de5e23505b07f910226
ENC_Euphony <2330458484@qq.com>
2025-06-30 21:50:14 +0800

    Png

--- common/src/main/java/com/euphony/better_client/utils/ConfigUtils.java
+++ common/src/main/java/com/euphony/better_client/utils/ConfigUtils.java
@@ -1,15 +1,58 @@
 package com.euphony.better_client.utils;
 
 import com.euphony.better_client.BetterClient;
-import dev.isxander.yacl3.api.Option;
-import dev.isxander.yacl3.api.OptionDescription;
+import dev.isxander.yacl3.api.*;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.server.packs.resources.Resource;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class ConfigUtils {
     public static final int IMG_WIDTH = 1920;
     public static final int IMG_HEIGHT = 991;
+
+    private static final Map<ResourceLocation, int[]> IMAGE_DIMENSIONS_CACHE = new HashMap<>();
+
+    public static final OptionFlag RESOURCE_RELOAD = (client) -> {
+        if(client.hasSingleplayerServer()) {
+            MinecraftServer server = client.getSingleplayerServer();
+            if (server != null) {
+                PackRepository packrepository = server.getPackRepository();
+                Collection<String> collection = packrepository.getSelectedIds();
+                server.reloadResources(collection);
+            }
+        }
+    };
+
+    public static ListOption.Builder<String> getListGroupOption(String name) {
+        return ListOption.<String>createBuilder()
+                .name(getOptionName(name))
+                .description(OptionDescription.createBuilder()
+                        .text(getDesc(name, null))
+                        .build()
+                );
+    }
+
+    public static ButtonOption.Builder getButtonOption(String name) {
+        return ButtonOption.createBuilder()
+                .name(getButtonOptionName(name))
+                .description(OptionDescription.createBuilder()
+                        .text(getDesc(name, null))
+                        .build()
+                );
+    }
 
     public static <T> Option.Builder<T> getGenericOption(String name) {
         return getGenericOption(name, (DescComponent) null);
@@ -29,14 +72,39 @@ public class ConfigUtils {
     }
 
     public static <T> Option.Builder<T> getGenericOption(String name, String image, DescComponent descComponent) {
+        int[] dimensions = getImageDimensions(Utils.prefix(String.format("config/%s.png", image)));
         return Option.<T>createBuilder()
                 .name(getOptionName(name))
                 .description(OptionDescription.createBuilder()
                         .text(getDesc(name, descComponent))
-                        .image(getImage(image), IMG_WIDTH, IMG_HEIGHT)
+                        .image(getImage(image), dimensions[0], dimensions[1])
                         .build()
                 );
     }
+
+    public static int[] getImageDimensions(ResourceLocation location) {
+        if (IMAGE_DIMENSIONS_CACHE.containsKey(location)) {
+            return IMAGE_DIMENSIONS_CACHE.get(location);
+        }
+
+        Minecraft mc = Minecraft.getInstance();
+
+        Optional<Resource> resource = mc.getResourceManager().getResource(location);
+        if(resource.isPresent()) {
+            try (InputStream inputStream = resource.get().open()) {
+                BufferedImage image = ImageIO.read(inputStream);
+                if (image != null) {
+                    int[] dimensions = new int[]{image.getWidth(), image.getHeight()};
+                    IMAGE_DIMENSIONS_CACHE.put(location, dimensions);
+                    return dimensions;
+                }
+            } catch (IOException e) {
+                return new int[]{IMG_WIDTH, IMG_HEIGHT};
+            }
+        }
+        return new int[]{IMG_WIDTH, IMG_HEIGHT};
+    }
+
 
     public static Component getCategoryName(String category) {
         return Component.translatable(String.format("yacl3.config.%s:config.category.%s", BetterClient.MOD_ID, category));
@@ -44,6 +112,10 @@ public class ConfigUtils {
 
     public static Component getGroupName(String category, String group) {
         return Component.translatable(String.format("yacl3.config.%s:config.category.%s.group.%s", BetterClient.MOD_ID, category, group));
+    }
+
+    private static Component getButtonOptionName(String option) {
+        return Component.translatable(String.format("yacl3.config.%s:config.%s.button", BetterClient.MOD_ID, option));
     }
 
     private static Component getOptionName(String option) {
