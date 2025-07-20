5f959e8b65cfed81bac63f62c00f0ed62da25df6
ENC_Euphony <2330458484@qq.com>
2025-07-21 02:48:24 +0800

    Update GameRendererMixin.java

--- common/src/main/java/com/euphony/better_client/mixin/GameRendererMixin.java
+++ common/src/main/java/com/euphony/better_client/mixin/GameRendererMixin.java
@@ -12,11 +12,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.nio.file.Path;
-import java.util.Optional;
 
 import static com.euphony.better_client.BetterClient.config;
 
