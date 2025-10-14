919b9a400325478ee5210bda310ef60944ece2ec
pynickle <2330458484@qq.com>
2025-10-15 00:02:21 +0800

    fix: fix image dimensions in config

--- common/src/main/java/com/euphony/better_client/utils/ConfigUtils.java
+++ common/src/main/java/com/euphony/better_client/utils/ConfigUtils.java
@@ -3,13 +3,25 @@ package com.euphony.better_client.utils;
 import com.euphony.better_client.BetterClient;
 import dev.isxander.yacl3.api.Option;
 import dev.isxander.yacl3.api.OptionDescription;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class ConfigUtils {
     public static final int IMG_WIDTH = 1920;
     public static final int IMG_HEIGHT = 991;
+
+    private static final Map<ResourceLocation, int[]> IMAGE_DIMENSIONS_CACHE = new HashMap<>();
 
     public static <T> Option.Builder<T> getGenericOption(String name) {
         return getGenericOption(name, (DescComponent) null);
@@ -29,11 +41,12 @@ public class ConfigUtils {
     }
 
     public static <T> Option.Builder<T> getGenericOption(String name, String image, DescComponent descComponent) {
+        int[] dimensions = getImageDimensions(Utils.prefix(String.format("config/%s.png", image)));
         return Option.<T>createBuilder()
                 .name(getOptionName(name))
                 .description(OptionDescription.createBuilder()
                         .text(getDesc(name, descComponent))
-                        .image(getImage(image), IMG_WIDTH, IMG_HEIGHT)
+                        .image(getImage(image), dimensions[0], dimensions[1])
                         .build()
                 );
     }
@@ -54,6 +67,29 @@ public class ConfigUtils {
         MutableComponent component = Component.translatable(String.format("yacl3.config.%s:config.%s.desc", BetterClient.MOD_ID, option));
         if (descComponent != null) component.append(Component.literal("\n\n").append(descComponent.getText()));
         return component;
+    }
+
+    public static int[] getImageDimensions(ResourceLocation location) {
+        if (IMAGE_DIMENSIONS_CACHE.containsKey(location)) {
+            return IMAGE_DIMENSIONS_CACHE.get(location);
+        }
+
+        Minecraft mc = Minecraft.getInstance();
+
+        Optional<Resource> resource = mc.getResourceManager().getResource(location);
+        if(resource.isPresent()) {
+            try (InputStream inputStream = resource.get().open()) {
+                BufferedImage image = ImageIO.read(inputStream);
+                if (image != null) {
+                    int[] dimensions = new int[]{image.getWidth(), image.getHeight()};
+                    IMAGE_DIMENSIONS_CACHE.put(location, dimensions);
+                    return dimensions;
+                }
+            } catch (IOException e) {
+                return new int[]{IMG_WIDTH, IMG_HEIGHT};
+            }
+        }
+        return new int[]{IMG_WIDTH, IMG_HEIGHT};
     }
 
     private static ResourceLocation getImage(String name) {
