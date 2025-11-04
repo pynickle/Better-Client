de296354b900c9babf6c38ac957c9af8ade5fda8
pynickle <2330458484@qq.com>
2025-11-04 21:06:34 +0800

    fix: fix suspicious stew tooltip not working

--- common/src/main/java/com/euphony/better_client/mixin/SuspiciousStewEffectsMixin.java
+++ common/src/main/java/com/euphony/better_client/mixin/SuspiciousStewEffectsMixin.java
@@ -1,23 +1,23 @@
 package com.euphony.better_client.mixin;
 
-import net.minecraft.core.component.DataComponentGetter;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.TooltipFlag;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.item.component.SuspiciousStewEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.function.Consumer;
 
 import static com.euphony.better_client.BetterClient.config;
 
 @Mixin(SuspiciousStewEffects.class)
 public class SuspiciousStewEffectsMixin {
-    @Inject(method = "addToTooltip", at = @At(value = "HEAD"), cancellable = true)
-    private void isCreative(Item.TooltipContext context, Consumer<Component> tooltipAdder, TooltipFlag flag, DataComponentGetter componentGetter, CallbackInfo ci) {
-        if (!config.enableSuspiciousStewTooltip) ci.cancel();
+    @ModifyExpressionValue(
+            method = "addToTooltip",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/TooltipFlag;isCreative()Z"
+            )
+    )
+    private boolean modifyIsCreative(boolean original) {
+        if (!config.enableSuspiciousStewTooltip) return original;
+        return true;
     }
 }
